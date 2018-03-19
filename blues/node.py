f6ebcf7f704744c910a3f90c63c3fed1b7a3e530
Carl Skeide <carl@skeide.se>
2018-03-19 13:47:20 +0100

    Update node blueprint to allow for more flexible installations

--- blues/node.py
+++ blues/node.py
@@ -12,6 +12,7 @@ Node.js Blueprint
     settings:
       node:
         # version: latest    # Install latest node version
+        # user: some_app_user
         packages:            # List of npm packages to install (Optional)
           # - coffee-script
           # - yuglify
@@ -59,16 +60,20 @@ def configure():
     install_packages()
 
 
-def get_version():
+def get_version(default=7):
     """
     Support only major versions, since it affects which repository to add.
     """
     version = blueprint.get('version')
-    if version in range(4, 8):
-        return version
+    try:
+        return int(version)
 
-    else:
+    except ValueError:
         abort('Unsupported node version: {}'.format(version))
+
+
+def get_user():
+    return blueprint.get('user')
 
 
 def install(for_user=None):
@@ -76,16 +81,14 @@ def install(for_user=None):
 
     with sudo():
         lsb_release = debian.lsb_release()
+        if lsb_release not in ('14.04', '16.04'):
+            abort('Unsupported OS version: {}'.format(lsb_release))
+
         codename = debian.lsb_codename()
         version = get_version()
 
-        if lsb_release in ('14.04', '16.04'):
-
-            repository = 'https://deb.nodesource.com/node_{}.x {} main'.format(version, codename)
-            debian.add_apt_repository(repository)
-
-        else:
-            abort('Unsupported OS version: {}'.format(lsb_release))
+        repository = 'https://deb.nodesource.com/node_{}.x {} main'.format(version, codename)
+        debian.add_apt_repository(repository)
 
         info('Adding apt key for', 'nodesource')
         debian.add_apt_key('https://deb.nodesource.com/gpgkey/nodesource.gpg.key')
@@ -99,10 +102,11 @@ def install_packages():
     packages = blueprint.get('packages', [])
     if packages:
         info('Installing Packages')
-        npm('install', *packages)
+        npm('install {}'.format(' '.join(packages)), user=get_user())
 
 
-def npm(command, *options):
-    info('Running npm {}', command)
-    with sudo():
-        run('npm {} -g {}'.format(command, ' '.join(options)))
+def npm(command, user='root', *args):
+    info('Running npm {} (as {})', command, user)
+    sudo = 'sudo -i' if user == 'root' else 'sudo -iu {}'.format(user)
+    opts = ' '.join(args)
+    run('{} npm {} {}'.format(sudo, opts, command))
