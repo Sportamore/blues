ee9a008859d369c3848b7e32f80530b0b68a0501
Teodor Engqvist <teodor.engqvist@sportamore.se>
2018-05-02 09:56:20 +0200

    Added sysctl to blues. Files under sysctl.d will just be uploaded to server.

---
+++ blues/sysctl.py
@@ -0,0 +1,90 @@
+"""
+BIND Blueprint
+===============
+
+**Fabric environment:**
+
+.. code-block:: yaml
+
+    blueprints:
+      - blues.bind
+
+    settings:
+      bind:
+        # listen:
+        #   - 127.0.0.1
+        # allow-query:
+        #   - 127.0.0.1
+        # allow-reqursion:
+        #   - 127.0.0.1
+        # forwarders:
+        #   - "8.8.8.8"
+        #   - "8.8.4.4"
+        zones:
+          - company.local
+          - some.other.zone
+
+"""
+import os
+from time import time
+
+from fabric.context_managers import cd, settings
+from fabric.contrib import files
+from fabric.decorators import task, parallel
+from fabric.api import put,env,local
+
+from refabric.context_managers import sudo, silent, hide_prefix
+from refabric.contrib import blueprints
+from refabric.operations import run
+from refabric.utils import info
+
+from . import debian
+
+__all__ = [ 'list', 'configure']
+
+
+blueprint = blueprints.get(__name__)
+
+config_dir = '/etc/'
+sysctl_dir = os.path.join(config_dir, 'sysctl.d/')
+path=os.getcwd()
+
+
+@task 
+@task 
+def list(lista=False):
+    """
+    List sysctl lista for one or more values sysctl.list:lista='vm.swappiness;vm.panic_on_oom'
+    """
+    with sudo():
+        if lista==False:
+            run("sysctl -a")
+        else :
+            lista2=lista.split(';')
+            values=[]
+            for l in lista2:
+                values.append(run("sysctl "+l))
+            for value in values: 
+                info(value)
+@task
+def configure():
+    """
+    Configure sysctl settings
+    """
+    with sudo():
+       
+        uploads = []
+
+        # Configure application
+        local_params=blueprint.get('params',[])
+        #for local_param,value in local_params:
+        print(local_params)
+        uploads.append(blueprint.upload('./sysctl.conf',config_dir,{"params" : local_params}))
+        info(path+'/templates/'+env.roles[0]+
+            '/sysctl/sysctl.d/*')
+        listan=[]
+        listan.append(local("ls "+path+'/templates/'+env.roles[0]+
+            '/sysctl/sysctl.d/',capture=True))
+        for fil in listan:
+            put(path+'/templates/'+env.roles[0]+'/sysctl/sysctl.d/'+fil, sysctl_dir+fil,use_sudo=True)
+        info("In order for the new settings to work you need to reboot the system !!")
