2f6a0ea96eaea44fa52fc16e1d6870fa57d2d99f
Carl Skeide <carl@skeide.se>
2018-09-19 10:31:11 +0200

    Add cron status task

--- blues/cron.py
+++ blues/cron.py
@@ -18,14 +18,14 @@ import os
 from fabric.decorators import task
 from fabric.utils import abort
 
-from refabric.context_managers import sudo, silent
+from refabric.context_managers import sudo, silent, hide_prefix
 from refabric.contrib import blueprints
 from refabric.operations import run
 from refabric.utils import info
 
 from blues import debian
 
-__all__ = ['configure', 'disable']
+__all__ = ['configure', 'disable', 'status']
 
 
 blueprint = blueprints.get(__name__)
@@ -44,6 +44,7 @@ def configure():
                 info('Installing new crontab for {}...', user)
                 run('crontab -u {} {}'.format(user, os.path.join(temp_dir, user)))
 
+
 @task
 def disable(user=None):
     """
@@ -55,3 +56,16 @@ def disable(user=None):
     with sudo():
         info('Disabling crontab for {}...', user)
         run('crontab -r -u {}'.format(user))
+
+
+@task
+def status(user=None):
+    """
+    Dumps the crontab for a single user
+    """
+    if not user:
+        abort('Please specify user account')
+
+    info('Current crontab for {}:', user)
+    with sudo(), hide_prefix():
+        run('crontab -l -u {}'.format(user))
